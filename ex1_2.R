9e0863aa2cd1a985e0051f098ef0700f2729b04f
d@ni€| <artchounindaniel@gmail.com>
2016-06-10 21:13:01 +0200

    Added linear regression without intercept in exercise 1.2.

--- ex1_2.R
+++ ex1_2.R
@@ -24,20 +24,36 @@ ztst <- data$ztst;
 
 
 print('* Test 1 *');
-results <- log.app(Xapp, zapp, TRUE, 1e-5)
-print(results);
-results2 <- log.val(results$beta, Xtst);
+print('- Régression linéaire (sans intercept) -');
+results1 <- log.app(Xapp, zapp, FALSE, 1e-5, TRUE)
+print(results1);
+results2 <- log.val(results1$beta, Xtst);
 print(results2);
 print(ztst);
 print(errorRate(results2$predictions, ztst));
 
 # Decision boundary
-pdf(file = "./images/ex1/logReg.pdf");
-prob.log(results$beta, Xtst, ztst, c(0.5));
+pdf(file = "./images/ex1/logRegWithoutIntercept.pdf");
+prob.log(results1$beta, Xtst, ztst, c(0.5));
+dev.off();
+
+
+print('- Régression linéaire (avec intercept) -');
+results3 <- log.app(Xapp, zapp, TRUE, 1e-5)
+print(results3);
+results4 <- log.val(results3$beta, Xtst);
+print(results4);
+print(ztst);
+print(errorRate(results4$predictions, ztst));
+
+# Decision boundary
+pdf(file = "./images/ex1/logRegWithIntercept.pdf");
+prob.log(results3$beta, Xtst, ztst, c(0.5));
 dev.off();
 
 
 print('* Test 2 *');
+print('- Régression quadratique -');
 results <- log.quad.app(Xapp, zapp, 1e-5)
 print(results);
 results2 <- log.quad.val(results$beta, Xtst);
